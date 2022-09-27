fe7a3324fdba9a392444dacb35f612f677dc6abf
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-09-27 19:08:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package dining_food_order;
 
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main extends Dining_HomePage {
 	public static void main(String[] args) {
@@ -21,15 +23,35 @@ public class Main extends Dining_HomePage {
 				Dining_HomePage dine = new Dining_HomePage();
 				Customer user_detail=new Customer();
 				System.out.println("Customer Name:");
-				user_detail.setName(sc1.next()) ;
-				System.out.println("Mobile Number:");
-				user_detail.setMobile(sc1.next());
+				String name=sc1.next();
+				user_detail.setName(name) ;
+				boolean isValid_contact=false;
+				while(!isValid_contact)
+				{
+					System.out.println("Mobile Number:");
+					String mobile_no=sc1.next();
+					isValid_contact=dine.validate_mobile_number(mobile_no);
+					if(isValid_contact)
+						{
+						user_detail.setMobile(mobile_no);
+						}
+					else
+						System.out.println("Invalid mobile number...enter valid mobile number");
+				}
 				dine.home_page(user_detail);
 			}
 				break;
 			case 2: {
-				System.out.println(
-						"Our Authentic Indian Cuisine Restaurant has been providing best quality food since 1990....");
+				System.out.println("Our Authentic Indian Cuisine Restaurant has been providing best quality food since 1990....");
+				System.out.println("We are providing Vegiterian and Non Vegiterian Indian cuisines to make provide people healthier food");
+				System.out.println("\n***************************************************************");
+				System.out.printf("%-10s %-10s %-10s"," ","Our Contact details"," ");
+				System.out.println("\n****************************************************************");
+				System.out.println("Authentic Indian Cuisine Restaurant");
+				System.out.println("GST Road,Potheri");
+				System.out.println("Contact Number: 9876543210");
+				System.out.println("Contact mail id:authentic_indian_cuisine@zohomail.com");
+				System.out.println("\n****************************************************************\n");
 			}
 				break;
 
@@ -39,7 +61,8 @@ public class Main extends Dining_HomePage {
 				break;
 
 			default: {
-				System.out.println("Invalid choice....");
+				System.out.println("We are not providing this service....Please try any of our mentioned services");
+				System.out.println("Thank you for UNDERSTANDING");
 			}
 			}
 		}
