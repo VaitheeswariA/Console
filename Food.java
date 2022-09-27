8075dba7630d29e9aa5f175a3636f1a28118002b
VaitheeswariA <111962400+VaitheeswariA@users.noreply.github.com>
2022-09-27 19:07:37 +0530

    Update Food.java

--- Food.java
+++ Food.java
@@ -3,7 +3,7 @@ package dining_food_order;
 import java.util.*;
 
 class Food {
-	Map<String, ArrayList<String>> menu = new LinkedHashMap<>();
+	//Map<String, ArrayList<String>> menu = new LinkedHashMap<>();
 	public static Map<Integer, LinkedHashMap<String, Double>> sub_menu = new LinkedHashMap<>();
 	public static LinkedHashMap<String, Double> veg = new LinkedHashMap<>();
 	public static LinkedHashMap<String, Double> non_veg = new LinkedHashMap<>();
@@ -38,7 +38,54 @@ class Food {
 		non_veg.put("Fish Masala", 200.00);
 		non_veg.put("Prawn gravy", 150.00);
 		non_veg.put("Butter Chicken masala", 150.00);
+		
+		
+		//rice menu details
+		rice.put("White basmati rice", 100.00);
+		rice.put("Ghee rice", 115.70);
+		rice.put("pulao", 110.00);
+		rice.put("Veg Briyani", 125.00);
+		rice.put("Chenna Briyani", 130.00);
+		rice.put("Prawn Briyani", 230.00);
+		rice.put("Chicken Briyani", 210.00);
+		rice.put("Mutton Briyani", 250.00);
+		rice.put("Fish Briyani", 230.00);
+		rice.put("Hydrabadhi Chicken briyani", 250.75);
+	
+		//bread menu details
+		bread.put("Naan", 20.00);
+		bread.put("Roti", 25.00);
+		bread.put("Chapathi", 15.00);
+		bread.put("Garlic naan", 23.00);
+		bread.put("Parotta", 17.00);
+		bread.put("cyleon parotta", 18.00);
+		bread.put("Chilli parotta", 110.00);
+		bread.put("Chicken kothu parotta", 130.00);
+		bread.put("Butter Naan", 22.00);
+		bread.put("kuboos", 15.00);
 
+		//drinking beverages menu details
+		drinking_beverages.put("Mint Mojito", 70.00);
+		drinking_beverages.put("Lemon Soda", 60.00);
+		drinking_beverages.put("Danger Soda", 170.00);
+		drinking_beverages.put("Coke", 40.00);
+		drinking_beverages.put("paneer soda", 40.00);
+		
+		
+		//desert menu list
+		desert.put("Gulab jamun", 40.00);
+		desert.put("Brownie", 50.00);
+		desert.put("Sizzling Brownie", 130.00);
+		desert.put("Carrot halwa", 70.00);
+		desert.put("Bread halwa", 50.00);
+		
+		//shakes menu details
+		shakes.put("Strawberry", 110.00);
+		shakes.put("Mango", 120.00);
+		shakes.put("Vennila", 100.00);
+		shakes.put("Chocolate", 120.00);
+		shakes.put("ButterFruit", 140.00);
+		
 		// Category details
 		sub_menu.put(1, veg);
 		sub_menu.put(2, non_veg);
@@ -48,14 +95,7 @@ class Food {
 		sub_menu.put(6, desert);
 		sub_menu.put(7, shakes);
 
-		menu.put("Rice", new ArrayList<String>(
-				Arrays.asList("White basmati rice", "Ghee rice", "pulao", "Chicken Briyani", "Mutton Briyani")));
-		menu.put("Bread", new ArrayList<String>(Arrays.asList("Naan", "Roti", "Chapathi", "Garlic naan", "Parotta")));
-		menu.put("Drinking beverages",
-				new ArrayList<String>(Arrays.asList("Mint Mojito", "Lemon Soda", "Danger Soda", "Coke")));
-		menu.put("Deserts", new ArrayList<String>(
-				Arrays.asList("Gulab jamun", "Brownie", "Sizzling Brownie", "Carrot halwa", "Bread halwa")));
-		menu.put("Shakes", new ArrayList<String>(Arrays.asList("Strawberry", "Mango", "Vennila", "Chocolate")));
+		//menu.put("Shakes", new ArrayList<String>(Arrays.asList("Strawberry", "Mango", "Vennila", "Chocolate")));
 
 	}
 
@@ -74,59 +114,97 @@ class Food {
 	}
 
 	public void showdishes(int category) {
+		int dish_index=0;
+		System.out.println();
 		switch (category) {
 		case 1: {
-			System.out.print("********Available varieties of dishes in veg Category***********");
+			System.out.println("--------Available varieties of dishes in veg Category-----------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : veg.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
+				
 			}
 		}
 			break;
 
 		case 2: {
-			System.out.print("********Available varieties of dishes in Non veg Category***********");
+			System.out.println("-------Available varieties of dishes in Non veg Category-------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : non_veg.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
 
 		case 3: {
-			System.out.print("********Available varieties of dishes in rice Category***********");
+			System.out.println("--------Available varieties of dishes in rice Category------------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : rice.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
 
 		case 4: {
-			System.out.print("********Available varieties of dishes in bread Category***********");
+			System.out.println("--------Available varieties of dishes in bread Category-----------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : bread.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
 
 		case 5: {
-			System.out.print("********Available varieties of dishes in drinking beverages Category***********");
+			System.out.println("-------Available varieties of dishes in drinking beverages Category-------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("------------------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : drinking_beverages.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
 
 		case 6: {
-			System.out.print("********Available varieties of dishes in desert Category***********");
+			System.out.println("-------Available varieties of dishes in desert Category-----------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : desert.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
 
 		case 7: {
-			System.out.print("********Available varieties of dishes in shakes Category***********");
+			System.out.println("-------Available varieties of dishes in shakes Category-----------");
+			System.out.printf("%-5s %-25s %-5s","S.No","Dish Name","Price Per Quantity");
+			System.out.println();
+			System.out.println("----------------------------------------------------------------");
+			System.out.println();
 			for (Map.Entry<String, Double> dish : shakes.entrySet()) {
-				System.out.println(dish.getKey() + "------------------------------------" + dish.getValue());
+				System.out.printf("%-5s %-25s %-5s",++dish_index,dish.getKey(),dish.getValue());
+				System.out.println();
 			}
 		}
 			break;
@@ -135,7 +213,8 @@ class Food {
 			return;
 
 		default:
-			System.out.println("Invalid category");
+			System.out.println("We are not providing this category of varieties....Please try any of our mentiond services");
+			System.out.println("Thank you for UNDERSTANDING:)");
 		}
 	}
 }
